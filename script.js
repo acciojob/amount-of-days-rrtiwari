0c79922d8918d71573a32addf7231fff835c1531
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-07-31 19:04:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-//your JS code here. If required.
-Function daysOfAYear(year){
-	if((year%4===0 && year%100!==0) ||(year%400===0)
-		return 366;
-	else
-		return 365;
+function daysOfAYear(year) {
+  year = parseInt(year);
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  }
+  return 365;
 }
+
+function checkDays() {
+  const year = prompt("Enter a year");
+  const days = daysOfAYear(year);
+  alert(days.toString());
+}
+
