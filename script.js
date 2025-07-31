e45f07d7adac1a6e77bf79824b8ce0a7884b1e22
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-07-31 18:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //your JS code here. If required.
 Function daysOfAYear(year){
-	if((year%4===0 && year%100!==0) ||(year%400===0){
+	if((year%4===0 && year%100!==0) ||(year%400===0)
 		return 366;
 	else
 		return 365;
-	}
 }
